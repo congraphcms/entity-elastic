3a288e97caccfcc9a1559d09e5c6d5d793e38377
Nikola Plavsic <nikolaplavisc@gmail.com>
2017-11-04 16:59:06 +0100

    config publishing fix

--- EntityElasticServiceProvider.php
+++ EntityElasticServiceProvider.php
@@ -45,8 +45,8 @@ class EntityElasticServiceProvider extends ServiceProvider {
 	 */
 	public function boot() {
 		$this->publishes([
-			__DIR__.'/config/config.php' => config_path('cb.elastic'),
-			__DIR__.'/config/eav.php' => config_path('cb.eav'),
+			__DIR__.'/config/config.php' => config_path('cb.elastic.php'),
+			__DIR__.'/config/eav.php' => config_path('cb.eav.php'),
 		]);
 	}
 
